f91c0a4b5a74afbc35d66f78f30e562a806b1b71
viktor <viktortat@gmail.com>
2023-09-28 00:44:45 +0300

    + up

--- DockerfileServer/server.py
+++ DockerfileServer/server.py
@@ -1,11 +1,18 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import socket
+import time
+import getpass
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        user = getpass.getuser()
+        host_ip = self.client_address[0]
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        response_text = f'Hello world from hostname: {socket.gethostname()}, Current Time: {current_time}, ' \
+                        f'username: {user}, server IP: {host_ip}'
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Hello world from hostname: ' + socket.gethostname().encode())
+        self.wfile.write(response_text.encode())
 
 SERVER_PORT = 8000
 httpd = HTTPServer(('0.0.0.0', SERVER_PORT), SimpleHTTPRequestHandler)
